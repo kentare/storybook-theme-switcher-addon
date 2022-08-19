7be872b4b39625fd03f25cd477dde0b3363d643d
kentare <kentaretorvik@gmail.com>
2022-08-19 14:16:45 +0200

    fix: fix path of preset.js entries

--- src/preset.js
+++ src/preset.js
@@ -1,9 +1,9 @@
 function config(entry = []) {
-  return [...entry, require.resolve("./preview")];
+  return [...entry, require.resolve("./preset/preview")];
 }
 
 function managerEntries(entry = []) {
-  return [...entry, require.resolve("./manager")];
+  return [...entry, require.resolve("./preset/manager")];
 }
 
 module.exports = {
